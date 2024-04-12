6f8309caf052a58276f6e5212f5ce58ad6dd6708
c9845 (AA) <c9845 (AA)>
2024-04-12 12:00:23 -0400

    Revise error handlers and add new ones to better catch error events that can be ignored.

--- error-handlers.go
+++ error-handlers.go
@@ -26,11 +26,13 @@ ignore/bypass errors returned from running a DeployQuery or UpdateQuery.
 //	 }
 type ErrorHandler func(string, error) bool
 
-// IgnoreErrorDuplicateColumn checks if an error occured because a column with the
+// IgnoreErrorDuplicateColumn checks if an error occurred because a column with the
 // same name already exists. This is useful to for running ALTER TABLE ADD COLUMN or
 // RENAME COLUMN.
 //
 // This error usually occurs because UpdateSchema() is being rerun.
+//
+//Ex.: ALTER TABLE my_table RENAME COLUMN old_column_name TO new_column_name.
 func IgnoreErrorDuplicateColumn(query string, err error) bool {
 	if strings.Contains(strings.ToUpper(query), "ADD COLUMN") && strings.Contains(strings.ToLower(err.Error()), "duplicate column") {
 		return true
@@ -39,21 +41,104 @@ func IgnoreErrorDuplicateColumn(query string, err error) bool {
 	return false
 }
 
-// IgnoreErrorDropUnknownColumn checks if an error occured because a column you are
-// trying to DROP has already been DROPped and does not exist.
+// IgnoreErrorDropColumn checks if an error occurred because a column you are trying
+// to DROP does not exist. This can occur if you already DROPped the column.
 //
 // This error usually occurs because UpdateSchema() is being rerun.
-func IgnoreErrorDropUnknownColumn(query string, err error) bool {
-	if strings.Contains(strings.ToUpper(query), "DROP COLUMN") {
-		//MariaDB.
-		if strings.Contains(err.Error(), "check that it exists") {
-			return true
-		}
+//
+//Ex.: ALTER TABLE my_table DROP COLUMN my_column.
+func IgnoreErrorDropColumn(query string, err error) bool {
+	if !strings.Contains(strings.ToUpper(query), "DROP COLUMN") {
+		return false
+	}
 
-		//SQLite.
-		if strings.Contains(err.Error(), "no such column") {
-			return true
-		}
+	//MariaDB.
+	if strings.Contains(err.Error(), "Error 1091") && strings.Contains(err.Error(), "check that it exists") {
+		return true
+	}
+
+	//SQLite.
+	if strings.Contains(err.Error(), "no such column") {
+		return true
+	}
+
+	return false
+}
+
+// IgnoreErrorDropForeignKey checks if an error occurred because a foreign key you
+// are trying to DROP does not exist. This can occur if you already DROPped the
+// foreign key.
+//
+// This error usually occurs because UpdateSchema() is being rerun.
+//
+//Ex.:
+// - MariaDB: ALTER TABLE my_table DROP FOREIGN KEY my_foreign_key.
+// - SQLite: You will have to turn foreign keys off, create a new table, and copy over
+//   data.
+func IgnoreErrorDropForeignKey(query string, err error) bool {
+	if !strings.Contains(strings.ToUpper(query), "DROP FOREIGN KEY") {
+		return false
+	}
+
+	//MariaDB.
+	if strings.Contains(err.Error(), "Error 1091") && strings.Contains(err.Error(), "check that it exists") {
+		return true
+	}
+
+	return false
+}
+
+// IgnoreErrorDropTable checks if an error occurred because a table you are trying to
+// DROP does not exist. This can occur if you already DROPped the table.
+//
+// This error usually occurs because UpdateSchema() is being rerun.
+//
+// Ex.: DROP TABLE my_table.
+func IgnoreErrorDropTable(query string, err error) bool {
+	if !strings.Contains(strings.ToUpper(query), "DROP TABLE") {
+		return false
+	}
+
+	//MariaDB.
+	if strings.Contains(err.Error(), "Error 1051") && strings.Contains(err.Error(), "Unknown table") {
+		return true
+	}
+
+	//SQLite.
+	if strings.Contains(err.Error(), "no such table") {
+		return true
+	}
+
+	return false
+}
+
+// IgnoreErrorTableDoesNotExist checks if an error occurred because you are trying to
+// modify a table in some manner but the table does not exist in the database.
+func IgnoreErrorTableDoesNotExist(query string, err error) bool {
+	//MariaDB.
+	if strings.Contains(err.Error(), "Error 1146") && strings.Contains(err.Error(), "Table") && strings.Contains(err.Error(), "doesn't exist") {
+		return true
+	}
+
+	//SQLite.
+	if strings.Contains(err.Error(), "no such table") {
+		return true
+	}
+
+	return false
+}
+
+// IgnoreErrorColumnDoesNotExist checks if an error occurred because you are trying to
+// update a column in some manner but the column does not exist.
+func IgnoreErrorColumnDoesNotExist(query string, err error) bool {
+	//MariaDB.
+	if strings.Contains(query, "UPDATE") && strings.Contains(err.Error(), "Unknown column") {
+		return true
+	}
+
+	//SQLite.
+	if strings.Contains(query, "UPDATE") && strings.Contains(err.Error(), "no such column") {
+		return true
 	}
 
 	return false
@@ -72,45 +157,6 @@ func IgnoreErrorSQLiteModify(query string, err error) bool {
 	//lint:ignore S1008 - i don't really like "return strings.Contains()", i don't think it is as clear.
 	if strings.Contains(query, "MODIFY COLUMN") {
 		return true
-	}
-
-	return false
-}
-
-// IgnoreErrorIndexAlreadyExists checks if an error occured because an index with the
-// given name already exists. If you use "IF NOT EXISTS" in your query this error will
-// not occur.
-func IgnoreErrorIndexAlreadyExists(query string, err error) bool {
-	if strings.Contains(query, "CREATE INDEX") {
-		//MariaDB.
-		if strings.Contains(err.Error(), "duplicate key name") {
-			return true
-		}
-
-		//SQLite.
-		if strings.Contains(err.Error(), "already exists") {
-			return true
-		}
-	}
-
-	return false
-}
-
-// IgnoreErrorDropDoesNotExist checks if an error occured because a column that does
-// not exists is trying to be dropped.
-//
-// This error usually occurs because UpdateSchema() is being rerun.
-func IgnoreErrorDropDoesNotExist(query string, err error) bool {
-	if strings.Contains(query, "DROP") {
-		//MariaDB.
-		if strings.Contains(err.Error(), "check that it exists") {
-			return true
-		}
-
-		//SQLite.
-		if strings.Contains(err.Error(), "unknown column") && !strings.Contains(err.Error(), "foreign key definition") {
-			return true
-		}
 	}
 
 	return false
